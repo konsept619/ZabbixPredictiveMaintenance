cfbfa378dde98c2800ce2330b1eb205a035a0bc9
konsept619 <konrad.wrzesien58@gmail.com>
2024-11-13 12:40:09 +0100

    Simple script to verify statistical properties of colleccted data

---
+++ response_distribution.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import csv
+import scipy.stats as stats
+import numpy as np
+
+data = []
+
+with open("./collected_data", 'r', newline='') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        value = float(row[1])
+        data.append(value)
+
+
+plt.figure(figsize=(10, 6))
+plt.hist(data, bins=50, color='skyblue', edgecolor='black', density=True)
+plt.title("Histogram of ICMP times")
+plt.xlabel("ICMP response time")
+plt.ylabel("Density of apperances")
+plt.grid()
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.kdeplot(data, fill=True, color='skyblue')
+plt.title("KDE of ICMP response times")
+plt.xlabel("Response time")
+plt.ylabel("Density of apperances")
+plt.grid()
+plt.show()
+
+
+
+gamma_params = stats.gamma.fit(data)
+lognorm_params = stats.lognorm.fit(data)
+
+
+x = np.linspace(min(data), max(data), 1000)
+gamma_pdf = stats.gamma.pdf(x, *gamma_params)
+lognorm_pdf = stats.lognorm.pdf(x, *lognorm_params)
+
+plt.figure(figsize=(10, 6))
+plt.hist(data, bins=50, density=True, alpha=0.6, color='skyblue', edgecolor='black', label='Histogram')
+#plt.plot(x, gamma_pdf, color='red', label='Gamma distribution')
+plt.plot(x, lognorm_pdf, color='green', label='Log-normal distribution')
+plt.title("Histogram with matched theoretical distributions")
+plt.xlabel("Response time")
+plt.ylabel("Density of probability")
+plt.legend()
+plt.grid()
+plt.show()
